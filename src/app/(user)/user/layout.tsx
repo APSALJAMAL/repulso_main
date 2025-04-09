00c8c6e85c4fdded25d512a1f11c379bf90b5dd4
APSALJAMAL <jamalapsal@gmail.com>
2025-04-09 10:13:29 +0530

    complete commit

--- src/app/(user)/user/layout.tsx
+++ src/app/(user)/user/layout.tsx
@@ -2,7 +2,8 @@ import DashboardNavbar from "@/components/dashboard/dashboard-navbar";
 import DashboardSidebar from "@/components/dashboard/dashboard-sidebar";
 import React from 'react';
 
-
+import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
 
 
 interface Props {
@@ -10,7 +11,27 @@ interface Props {
 }
 
 const DashboardLayout =async ({ children }: Props) => {
-   
+    const user = await currentUser();
+
+  if (!user) {
+    return null;
+  }
+
+  const existingUser = await prisma.user.findUnique({
+    where: { clerkId: user.id },
+  });
+
+  if (!existingUser) {
+    await prisma.user.create({
+        data: {
+          name: `${user.firstName ?? ""} ${user.lastName ?? ""}`,
+          clerkId: user.id,
+          email: user.emailAddresses[0].emailAddress,
+          avatar: user.imageUrl,
+        },
+      });
+      
+  }
     return (
         <div className="flex flex-col min-h-screen w-full">
             <DashboardNavbar />
